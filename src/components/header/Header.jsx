2bf303bc3c95a8a4c9daeb4833f424fde1335bbc
hassamkazmi <hassamsyed7@gmail.com>
2022-11-09 21:23:57 +0500

    response111

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -15,7 +15,7 @@ import "react-date-range/dist/theme/default.css"; // theme css file
 import { format } from "date-fns";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import '../CSS/mediaquery.css'
+import '../CSS/mediaquery.css';
 
 const Header = ({ type }) => {
   const [destination, setDestination] = useState("");
@@ -48,60 +48,60 @@ const Header = ({ type }) => {
     });
   };
 
-  const dateA = format(new Date(date[0].startDate), 'yyyy-MM-dd')
-  const dateD = format(new Date(date[0].endDate), 'yyyy-MM-dd')
+  const dateA = format(new Date(date[0].startDate), 'yyyy-MM-dd');
+  const dateD = format(new Date(date[0].endDate), 'yyyy-MM-dd');
   const ApiSubmission = async (data) => {
-      const options = {
-        method: "GET",
-        url: "https://apidojo-booking-v1.p.rapidapi.com/properties/list",
-        params: {
-          offset: "1",
-          arrival_date: dateA,
-          departure_date: dateD,
-          guest_qty: optionsgest.adult,
-          dest_ids: "-3712125",
-          room_qty: data.room_qty,
-          search_type: destination ,
-          children_qty: optionsgest.children,
-          children_age: '4',
-          search_id: "none",
-          order_by: data.orderby,
-          languagecode: "en-us",
-          price_filter_currencycode: "USD",
-          travel_purpose: data.travel_purpose,
-        },
-        headers: {
-          'X-RapidAPI-Key': 'e7384013famsh160f4f11514b472p1d214fjsnaa921a9fe1f0',
-          "X-RapidAPI-Host": "apidojo-booking-v1.p.rapidapi.com",
-        },
-      };
-  
-      return await axios
-        .request(options)
-        .then(function (response) {
-          setDataonTable(response.data.result);
-          setSearch(response.data);
-    
-  
-          return response.data.result;
-        })
-        .catch(function (error) {
-          console.error(error);
-        });
-    };
-    const handleSearch = async (data, event) => {
-      await ApiSubmission(data);
-      setDataonTable(await ApiSubmission(data));
-      navigate("/hotels", {
-        state: {
-          children_qty: optionsgest.children,
-          dateD: dateD,
-          dateA: dateA,
-        },
-      });
+    const options = {
+      method: "GET",
+      url: "https://apidojo-booking-v1.p.rapidapi.com/properties/list",
+      params: {
+        offset: "1",
+        arrival_date: dateA,
+        departure_date: dateD,
+        guest_qty: optionsgest.adult,
+        dest_ids: "-3712125",
+        room_qty: data.room_qty,
+        search_type: destination,
+        children_qty: optionsgest.children,
+        children_age: '4',
+        search_id: "none",
+        order_by: data.orderby,
+        languagecode: "en-us",
+        price_filter_currencycode: "USD",
+        travel_purpose: data.travel_purpose,
+      },
+      headers: {
+        'X-RapidAPI-Key': 'e7384013famsh160f4f11514b472p1d214fjsnaa921a9fe1f0',
+        "X-RapidAPI-Host": "apidojo-booking-v1.p.rapidapi.com",
+      },
     };
 
-    const submitdisable = dateD === dateA || destination === ''
+    return await axios
+      .request(options)
+      .then(function (response) {
+        setDataonTable(response.data.result);
+        setSearch(response.data);
+
+
+        return response.data.result;
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
+  const handleSearch = async (data, event) => {
+    await ApiSubmission(data);
+    setDataonTable(await ApiSubmission(data));
+    navigate("/hotels", {
+      state: {
+        children_qty: optionsgest.children,
+        dateD: dateD,
+        dateA: dateA,
+      },
+    });
+  };
+
+  const submitdisable = dateD === dateA || destination === '';
   return (
     <div className="header">
       <div
@@ -138,7 +138,7 @@ const Header = ({ type }) => {
             </h1>
             <p className="headerDesc">
               Get rewarded for your travels – unlock instant savings of 10% or
-              more with a free Lamabooking account
+              more with a free  booking account
             </p>
             <div className="headerSearch">
               <div className="headerSearchItem">
@@ -247,7 +247,7 @@ const Header = ({ type }) => {
                   </div>
                 )}
               </div>
-              
+
               <div className="headerSearchItem">
                 <button className="headerBtn submiheaderBtn" disabled={submitdisable} onClick={handleSearch}>
                   Search
